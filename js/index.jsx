5752ce9bece8d970d9804ffd02435b91dc70bc11
Rich Garcia <rchgrca@yahoo.com>
2017-04-29 05:12:23 -0700

    initial commit

---
+++ js/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import axios from 'axios'
+
+export default class App extends Component {
+  render() {
+    return (
+        <div>
+            <div className="clearfix border">
+                <section className="sm-col sm-col-4 border">
+                    left
+                </section>
+                <section className="sm-col sm-col-8 border">
+                    right
+                </section>
+            </div>
+        </div>
+    )
+  }
+}
+
+render(<App/>, document.getElementById('app'));
